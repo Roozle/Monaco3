e144cc40ebfec4309ec97b88a8c62348bdd047be
Roozle <tutoeth@gmail.com>
2019-04-20 22:26:32 +0100

    Add project files.

---
+++ Monaco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monaco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
